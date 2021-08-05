d185993e4d8b12e078449fd605dc09d72fb3c747
Douglas Cogubum <doug.cogubum@gmail.com>
2021-08-04 21:25:42 -0300

    Initial commit

---
+++ src/main/java/com/genekz/jpa/CourseSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.genekz.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSpringDataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseSpringDataJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/genekz/jpa/CourseSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genekz.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSpringDataJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
